94acc5f8f62098c8b5ac3c24951232012393dab2
Sahar Mirtalebi <s.mirtalebi@bluteam.com>
2026-06-25 17:15:11 +0330

    add auth middleware

--- delivery/httpserver/userhandler/handler.go
+++ delivery/httpserver/userhandler/handler.go
@@ -10,12 +10,14 @@ type Handler struct {
 	authSvc       authservice.Service
 	userSvc       userservice.Service
 	userValidator uservalidator.Validator
+	signKey       []byte
 }
 
-func New(authSvc authservice.Service, userSvc userservice.Service, userValidator uservalidator.Validator) Handler {
+func New(authSvc authservice.Service, userSvc userservice.Service, userValidator uservalidator.Validator, signKey string) Handler {
 	return Handler{
 		authSvc:       authSvc,
 		userSvc:       userSvc,
 		userValidator: userValidator,
+		signKey:       []byte(signKey),
 	}
 }
--- delivery/httpserver/userhandler/profile.go
+++ delivery/httpserver/userhandler/profile.go
@@ -3,20 +3,19 @@ package userhandler
 import (
 	"net/http"
 
+	"github.com/golang-jwt/jwt/v4"
 	"github.com/labstack/echo/v4"
 	"github.com/sahar-mirtalebi/quiz-battle/param"
 	"github.com/sahar-mirtalebi/quiz-battle/pkg/httpmessage"
 )
 
 func (h Handler) userProfile(c echo.Context) error {
-	bearerToken := c.Request().Header["Authorization"]
+	token := c.Get("user").(*jwt.Token)
+	claims := token.Claims.(jwt.MapClaims)
 
-	claims, err := h.authSvc.ParseJWT(bearerToken)
-	if err != nil {
-		return echo.NewHTTPError(http.StatusUnauthorized, err.Error())
-	}
+	userID := uint(claims["user_id"].(float64))
 
-	resp, err := h.userSvc.Profile(param.ProfileRequest{UserID: claims.UserID})
+	resp, err := h.userSvc.Profile(param.ProfileRequest{UserID: userID})
 	if err != nil {
 		msg, code := httpmessage.Error(err)
 		return echo.NewHTTPError(code, msg)
